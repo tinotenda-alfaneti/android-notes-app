2a35c7705b5ac745a1fc7ef7e6b72d02dcdcf34b
tinotenda-alfaneti <tinotenda.alfaneti@ashesi.edu.gh>
2023-03-08 12:10:43 +0000

    Step 13: Add button to go to new note

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -36,6 +36,12 @@ class _NotesViewState extends State<NotesView> {
       appBar: AppBar(
         title: const Text("Your Notes"),
         actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(newNoteRoute);
+            },
+            icon: const Icon(Icons.add),
+          ),
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
               switch (value) {
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -125,7 +125,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user = AuthUser(isEmailVerified: false, email: 'foo@gmail.com');
     _user = user;
     return Future.value(user);
   }
@@ -145,6 +145,7 @@ class MockAuthProvider implements AuthProvider {
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
       isEmailVerified: true,
+      email: 'foo@gmail.com',
     );
     _user = newUser;
   }
